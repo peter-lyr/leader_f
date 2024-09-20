f9c60fd2e00328af197fcd63e2c69495694e0740
peter-lyr <llydrp.ldp@gmail.com>
2024-09-21 00:40:34 +0800

    leader ff

--- plugin/leader_f.lua
+++ plugin/leader_f.lua
@@ -1,4 +1,5 @@
 require 'which-key'.register {
   ['<leader>f'] = { name = 'lsp', },
   ['<leader>fw'] = { function() vim.cmd 'ClangdSwitchSourceHeader' end, 'lsp: ClangdSwitchSourceHeader', mode = { 'n', 'v', }, },
+  ['<leader>ff'] = { function() vim.lsp.buf.format() end, 'lsp: format', mode = { 'n', 'v', }, },
 }
