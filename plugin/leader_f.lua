97075b3742ff69f9e70ece0714a2a1324f1749f7
peter-lyr <llydrp.ldp@gmail.com>
2024-09-30 23:10:01 +0800

    <leader>fp F.format_paragraph()

--- plugin/leader_f.lua
+++ plugin/leader_f.lua
@@ -1,5 +1,8 @@
+local F = require 'f'
+
 require 'which-key'.register {
   ['<leader>f'] = { name = 'lsp', },
   ['<leader>fw'] = { function() vim.cmd 'ClangdSwitchSourceHeader' end, 'lsp: ClangdSwitchSourceHeader', mode = { 'n', 'v', }, },
   ['<leader>ff'] = { function() vim.lsp.buf.format() end, 'lsp: format', mode = { 'n', 'v', }, },
+  ['<leader>fp'] = { function() F.format_paragraph() end, 'lsp: format paragraph', mode = { 'n', 'v', }, },
 }
