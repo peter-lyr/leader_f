4524a246086b86662108a5ce68d1be2fde9b544c
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 19:29:15 +0800

    <leader>fk <cmd>AerialPrev<cr>

--- plugin/leader_f.lua
+++ plugin/leader_f.lua
@@ -5,6 +5,11 @@ require 'which-key'.register {
   ['<leader>fw'] = { function() vim.cmd 'ClangdSwitchSourceHeader' end, 'lsp: ClangdSwitchSourceHeader', mode = { 'n', 'v', }, },
   ['<leader>ff'] = { function() vim.lsp.buf.format() end, 'lsp: format', mode = { 'n', 'v', }, },
   ['<leader>fp'] = { function() F.format_paragraph() end, 'lsp: format paragraph', mode = { 'n', 'v', }, },
-  ['<leader>fl'] = { function() F.lsp_document_symbols() end, 'lsp: lsp_document_symbols', mode = { 'n', 'v', }, },
+
   ['<leader>fr'] = { function() F.lsp_references() end, 'lsp: lsp_references', mode = { 'n', 'v', }, },
+  ['<leader>fl'] = { function() F.lsp_document_symbols() end, 'lsp: lsp_document_symbols', mode = { 'n', 'v', }, },
+  ['<leader>fo'] = { '<cmd>AerialNavToggle<cr>', 'lsp: AerialNavToggle', mode = { 'n', 'v', }, },
+
+  ['<leader>fj'] = { '<cmd>AerialNext<cr>', 'lsp: AerialNext', mode = { 'n', 'v', }, },
+  ['<leader>fk'] = { '<cmd>AerialPrev<cr>', 'lsp: AerialPrev', mode = { 'n', 'v', }, },
 }
